c9f02390601218005de2727da1c6da6ea22ee59d
rob-sys <robertas.sys@gmail.com>
2020-07-08 11:51:22 +0300

    fix it tests

--- src/it/scala/blog/iamrob/SparkJobTestIT.scala
+++ src/it/scala/blog/iamrob/SparkJobTestIT.scala
@@ -1,39 +1,43 @@
 package blog.iamrob
 
-import org.apache.spark.sql.{Dataset, SparkSession}
-import org.scalatest.FlatSpec
 import blog.iamrob.jobs._
-import blog.iamrob.test.SharedSparkSession.spark
+import org.scalatest.FlatSpec
 import org.apache.spark.sql.Row
 import blog.iamrob.storage.{Storage}
+import blog.iamrob.test.SharedSparkSession.spark
+import org.apache.spark.sql.{Dataset, SparkSession, DataFrameWriter}
+
 
 class SparkJobTestIT extends FlatSpec {
 
   class TestStorage(spark: SparkSession) extends Storage {
     import spark.implicits._
 
-    override def read(path: String): Dataset[Row] = spark
+    override def read(path: String, format: String): Dataset[Row] = spark
       .read
       .option("header", "true")
       .option("inferSchema", "true")
       .option("delimiter", ",")
       .csv(path)
 
-    override def write(ds: Dataset[_], path: String): Unit = {}
+    override def write(
+      ds: Dataset[_], path: String,
+      format: String, mode: String,
+      formatWriter: DataFrameWriter[_] => DataFrameWriter[_]): Unit = {}
   }
 
   "TripMetrics" should "read from disk" in {
-    val config = UsageConfig("src/it/resources/tripdata.csv")
+    val config = UsageConfig("src/it/resources/tripdata.csv", "csv")
     TripMetrics.run(spark, config, new TestStorage(spark))
   }
 
   "TripTop" should "read and write to disk" in {
-    val config = UsageConfig("src/it/resources/tripdata.csv")
+    val config = UsageConfig("src/it/resources/tripdata.csv", "csv")
     TripTop.run(spark, config, new TestStorage(spark))
   }
 
   "TripNeighbourhoodTop" should "read and write to disk"  in {
-    val config = UsageConfig("src/it/resources/tripdata.csv")
+    val config = UsageConfig("src/it/resources/tripdata.csv", "csv")
     TripNeighbourhoodTop.run(spark, config, new TestStorage(spark))
   }
 }
